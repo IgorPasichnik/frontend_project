19d7b497f5f3ee469a1a61c330969f2fef8458f0
IgorPasichnik <nekto.igoryambus@gmail.com>
2024-02-28 17:09:49 +0300

    test

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-// export default () => {
-//   const amountInput = document.querySelector("#amount");
-//   const depositBtn = document.querySelector("#depositBtn");
-//   const messageDiv = document.querySelector(".message");
-
-//   const isAmountValid = (amount) => {
-//     return /^\d+$/.test(amount);
-//   };
-
-//   depositBtn.addEventListener("click", function (event) {
-//     event.preventDefault();
-
-//     const amount = amountInput.value.trim();
-
-//     if (isAmountValid(amount) && parseInt(amount) > 0) {
-//       messageDiv.textContent = "Deposit successful";
-
-//       setTimeout(() => (messageDiv.textContent = ""), 5000);
-//     } else {
-//       messageDiv.textContent = "Введите допустимое положительное целое число.";
-//     }
-//   });
-// };
-
 export default () => {
   const amountInput = document.querySelector("#amount");
   const depositBtn = document.querySelector("#depositBtn");
